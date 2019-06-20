f4e4d6ce14460c4eb5b603f03116239667bcd90f
Dan Ortega <ortega.dan2010@gmail.com>
2019-06-19 22:53:25 -0500

    Our Team Activity Working ... the form is in root ("/") so you just go to "localhost:5000/" and everything will work from there

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    // when the user goes to the root redirect to form.ejs
+    .get('/', (req, res) => {
+        res.render('pages/form')
+    })
+    // when the url goes to /math do the math and then go to domath.ejs 
+    // ... done on purpose for the sake of learning
+    .get('/math', (req, res) => {
+
+        var num1 = req.query.num1;
+        var num2 = req.query.num2;
+        var operand = req.query.operand;
+
+        var operation = num1 + operand + num2
+
+        var answer = eval(operation)
+
+        console.log(operation + " = " + answer)
+
+        res.locals.answer = answer;
+
+        res.render('pages/domath')
+            // We could also do res.render('pages/domath', object)
+            // and the members of the object will be directly visible within the domath.ejs file
+            // therefore we could pass all the input from the request by passing req.query as the object
+            // doing res.render('pages/domath', res.query)
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
