93000719125a87b7210d61343e6170dfecde77d1
Dan Ortega <ortega.dan2010@gmail.com>
2019-06-22 21:49:49 -0500

    Testing how to serve static content

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+    // .use('/staticow', express.static('/home/danort/Documents'))
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
