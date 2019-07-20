2b2f28a31ab40d88ee288d3ba728c5a5a622ae02
Dan Ortega <ortega.dan2010@gmail.com>
2019-07-20 14:04:01 -0500

    Getting styles to work from EJS files, Lauch project from any file, and Jquey Ajax request successful from End-to-End

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,3 +2,14 @@ function clicked() {
     alert("You will be redirected by using JavaScript to the assignments page.\n Thank you for comming !!");
     window.location.href = 'assignments.php';
 }
+
+
+function getFammyUsers() {
+
+    // var URL = 'http://localhost:5000/team10/person/DANPAPA'
+    var URL = 'team10/person/DANPAPA'
+    $.get(URL, (data, status) => {
+        console.log(data, status)
+    })
+
+}
