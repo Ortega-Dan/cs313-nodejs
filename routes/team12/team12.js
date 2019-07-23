2db23f20492948aadd24afd49360f040f8752740
Dan Ortega <ortega.dan2010@gmail.com>
2019-07-23 18:23:02 -0500

    Saving username to the session in the router

--- routes/team12/team12.js
+++ routes/team12/team12.js
@@ -1,13 +1,22 @@
 var express = require('express')
 var router = express.Router()
 
+var session = require('express-session')
+
+
+router.use(session({
+    secret: 'I love my wife and daughters',
+    resave: false,
+    saveUninitialized: true
+}))
+
 // RECEIVE DATA AS JSON to use it here in the req.body object
 router.use(express.json())
 
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
-        console.log('Team Week 12 - Resquested at Time: ', Date.now())
+        console.log('Team Week 12 Middleware - Accessed at Time: ', Date.now())
         next()
     })
     // define the home page route
@@ -26,8 +35,10 @@ router.post('/login', (req, res) => {
     console.log('Received...\nUser: ' + uname)
     console.log('Password: ' + pword)
 
-    if (uname === "admin" && pword === "password")
+    if (uname === "admin" && pword === "password") {
         response = true
+        req.session.user = uname
+    }
 
     res.send({ success: response })
 })
